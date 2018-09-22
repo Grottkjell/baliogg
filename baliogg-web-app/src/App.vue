622d43e7a797c13fb32f340d490bde0c1af310ac
Emrik L <grottkjell@gmail.com>
2018-09-22 18:33:42 +0200

    Basic blog layout.

--- baliogg-web-app/src/App.vue
+++ baliogg-web-app/src/App.vue
@@ -1,19 +1,46 @@
 <template>
   <div id="baliogg-app">
-    <posts></posts>
+    <div class="banner">
+      <h1>Welcome to Baliogg, Emrik and Sannas blog of our trip to Bali, enjoy!</h1>
+    </div>
+    <posts class="content"></posts>
   </div>
 </template>
 
 <script>
-import Posts from './components/Posts.vue'
+import Posts from "./components/Posts.vue";
 
 export default {
-  name: 'baliogg-app',
+  name: "baliogg-app",
   components: {
     Posts
   }
-}
+};
 </script>
 
 <style>
+body {
+  background-color: #ebfdfe;
+  font-family: "Roboto Mono", monospace;
+  text-align: center;
+  margin: 0;
+}
+.banner {
+  background-color: #d5fafd;
+  border-bottom-left-radius: 2px;
+  border-bottom-right-radius: 2px;
+  box-shadow: 5px 0px 10px 0px;
+  padding: 10px 0;
+}
+.content {
+  display: grid;
+  grid-template-columns: 100%;
+  padding-top: 50px;
+}
+
+@media only screen and (min-width: 800px) {
+  .content {
+    grid-template-columns: 25% 50% 25%;
+  }
+}
 </style>
--- baliogg-web-app/src/components/Posts.vue
+++ baliogg-web-app/src/components/Posts.vue
@@ -1,15 +1,59 @@
 <template>
-  <div>This is where the posts should be presented!</div>
+  <div>
+    <div class="post" v-for="post in posts" v-bind:key="post.uploadDate">
+      <h2>{{post.title}}</h2>
+      <img v-if="post.image" v-bind:src="post.image">
+      <p v-if="post.text">{{post.text}}</p>
+      <p><i>{{post.uploadDate | dateFormat}}</i></p>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Posts",
-  props: {
-    msg: String
+  data: function() {
+    return {
+      posts: []
+    };
+  },
+  created: function() {
+    this.getPosts();
+  },
+  methods: {
+    getPosts: function() {
+      this.message = "Loading blog posts...";
+      axios
+        .get("/baliogg/api/post")
+        .then(response => {
+          this.posts = response.data;
+        })
+        .catch(() => {
+          this.message =
+            "Error loading blog posts..., contact admin if problem persists!";
+        });
+    }
+  },
+  filters: {
+    dateFormat: function(date) {
+      if (!date) {
+        return "";
+      }
+      return new Date(date).toLocaleDateString();
+    }
   }
 };
 </script>
 
 <style scoped>
+.post {
+  grid-column-start: 2;
+  grid-column-end: 3;
+  padding: 25px 0;
+}
+.post img {
+  max-width: 100%;
+}
 </style>
