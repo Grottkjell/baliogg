68fd2d68a9043f8eb0b9bd041a6d4c135ee519fe
Emrik L <grottkjell@gmail.com>
2018-09-24 21:28:39 +0200

    Sorted blog posts.

--- baliogg-web-app/src/components/Posts.vue
+++ baliogg-web-app/src/components/Posts.vue
@@ -3,7 +3,7 @@
     <div v-if="message">
       {{message}}
     </div>
-    <div class="post" v-for="post in posts" v-bind:key="post.uploadDate">
+    <div class="post" v-for="post in postsSortedByDate" v-bind:key="post.uploadDate">
       <h2>{{post.title}}</h2>
       <img v-for="(image, key) in post.image" v-bind:key="key" v-bind:src="'images/' + image">
       <p v-if="post.text">{{post.text}}</p>
@@ -39,8 +39,23 @@ export default {
           this.message =
             "Error loading blog posts, contact admin if problem persists!";
         });
-    }
+    },
   },
+  computed: {
+    postsSortedByDate: function() {
+      if (this.posts) {
+        return this.posts.sort((p1, p2) => {
+          if (p1.uploadDate && p2.uploadDate) {
+            return p1.uploadDate > p2.uploadDate ? -1 : 1;
+          } else {
+            return 0;
+          }
+        });
+      } else {
+        return this.posts;
+      }
+    }
+  },  
   filters: {
     dateFormat(date) {
       if (!date) {
@@ -53,13 +68,13 @@ export default {
 </script>
 
 <style scoped>
-  .post img {
-    max-width: 100%;
-  }
-  .post:not(:last-of-type) {
-    margin-bottom: 200px;
-  }
-  .post:not(:first-of-type) {
-    border-top: 1px solid gray;
-  }
+.post img {
+  max-width: 100%;
+}
+.post:not(:last-of-type) {
+  margin-bottom: 200px;
+}
+.post:not(:first-of-type) {
+  border-top: 1px solid gray;
+}
 </style>
